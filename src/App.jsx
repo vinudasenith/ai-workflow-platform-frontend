3bad982137936229699cacb364527e3997ccda45
vinudasenith <ha.vinudas@gmail.com>
2025-08-22 14:51:53 +0530

    add about,feature routes✌️

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import Dashboard from './pages/dashboard/dashboard';
 import AdminDashboard from './pages/admin-dashboard/adminDashboard';
 import Header from './components/header';
 import Home from './pages/home/home';
+import Features from './pages/home/feature';
+import About from './pages/home/about';
 
 
 function AppWrapper() {
   const location = useLocation()
-  const showHeader = location.pathname === "/";
+  const showHeader = location.pathname === "/" || location.pathname === "/feature" || location.pathname === "/about";
 
   return (
     <>
@@ -25,6 +27,9 @@ function AppWrapper() {
         <Route path="/uregister" element={<UserRegister />}></Route>
 
         <Route path="/" element={<Home />}></Route>
+        <Route path="/feature" element={<Features />}></Route>
+        <Route path="/about" element={<About />}></Route>
+
         <Route path="/dashboard/:tenantId" element={<Dashboard />}></Route>
         <Route path="/admin/dashboard" element={<AdminDashboard />}></Route>
       </Routes>
