a4d54a900e4480fb0b0b201c4f173b5221397789
vinudasenith <ha.vinudas@gmail.com>
2025-08-22 10:48:46 +0530

    add conditional header logic✌️

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, useLocation } from 'react-router-dom';
 import RegisterPage from './pages/register/register';
 import LoginPage from './pages/login/login';
 import UserLogin from './pages/login/userLogin';
@@ -6,24 +6,39 @@ import UserRegister from './pages/register/userRegister';
 import { Toaster } from 'react-hot-toast'
 import Dashboard from './pages/dashboard/dashboard';
 import AdminDashboard from './pages/admin-dashboard/adminDashboard';
+import Header from './components/header';
+import Home from './pages/home/home';
 
 
+function AppWrapper() {
+  const location = useLocation()
+  const showHeader = location.pathname === "/";
 
-
-function App() {
   return (
-    <BrowserRouter>
+    <>
       <Toaster position="top-right" reverseOrder={false} />
+      {showHeader && <Header />}
       <Routes path="/*">
         <Route path="/register" element={<RegisterPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/ulogin" element={<UserLogin />}></Route>
         <Route path="/uregister" element={<UserRegister />}></Route>
+
+        <Route path="/" element={<Home />}></Route>
         <Route path="/dashboard/:tenantId" element={<Dashboard />}></Route>
         <Route path="/admin/dashboard" element={<AdminDashboard />}></Route>
       </Routes>
-    </BrowserRouter>
+    </>
+
   )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppWrapper />
+    </BrowserRouter>
+  );
 }
 
 export default App;
