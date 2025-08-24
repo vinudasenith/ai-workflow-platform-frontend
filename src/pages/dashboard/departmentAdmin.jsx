08d76524e68cd7920b3b9748885e54e0a8fd97bf
vinudasenith <ha.vinudas@gmail.com>
2025-08-24 21:41:36 +0530

    add operation link to sidebar✌️

--- src/pages/dashboard/departmentAdmin.jsx
+++ src/pages/dashboard/departmentAdmin.jsx
@@ -13,6 +13,8 @@ export default function AdminDepartment() {
 
                     <a href="/dashboard/:tenantId/departmentAdmin/userManagment" className="block px-4 py-2 rounded-lg text-gray-700 hover:bg-indigo-50 hover:text-indigo-600 font-medium">User Management</a>
 
+                    <a href="/dashboard/:tenantId/departmentAdmin/operations" className="block px-4 py-2 rounded-lg text-gray-700 hover:bg-indigo-50 hover:text-indigo-600 font-medium">Operations</a>
+
                     <a href="/dashboard/:tenantId/departmentAdmin/notification" className="block px-4 py-2 rounded-lg text-gray-700 hover:bg-indigo-50 hover:text-indigo-600 font-medium">Communication & Notifications</a>
 
                     <a href="/dashboard/:tenantId/departmentAdmin/settings" className="block px-4 py-2 rounded-lg text-gray-700 hover:bg-indigo-50 hover:text-indigo-600 font-medium">Settings</a>
