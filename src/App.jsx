8ac38c78de8e749be37952af2338ebd956c9ce6b
vinudasenith <ha.vinudas@gmail.com>
2025-08-26 17:26:46 +0530

    Add staff dashboard routes for task management and operations✅

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,10 @@ import AdminUsersManagment from './pages/admin-dashboard/adminUsersManagment';
 import AdminDepartment from './pages/dashboard/departmentAdmin';
 import DepartmentManagment from './pages/department-admin-dashboard/departmentManagment';
 import UserManagment from './pages/department-admin-dashboard/userManagment';
-import Operations from './pages/department-admin-dashboard/operation';
+import Operations from './pages/staff-dashboard/operation';
+import StaffAdmin from './pages/dashboard/staff';
+import TaskManagment from './pages/staff-dashboard/taskManagment';
+import Task from './pages/staff-dashboard/task';
 
 
 function AppWrapper() {
@@ -27,25 +30,46 @@ function AppWrapper() {
       <Toaster position="top-right" reverseOrder={false} />
       {showHeader && <Header />}
       <Routes path="/*">
+
+        {/* Authentication */}
         <Route path="/register" element={<RegisterPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/ulogin" element={<UserLogin />}></Route>
         <Route path="/uregister" element={<UserRegister />}></Route>
 
+        {/* Home */}
         <Route path="/" element={<Home />}></Route>
         <Route path="/feature" element={<Features />}></Route>
         <Route path="/about" element={<About />}></Route>
 
+
+
+        {/* Dashboard */}
         <Route path="/dashboard/:tenantId" element={<Dashboard />}></Route>
         <Route path="/dashboard/:tenantId/departmentAdmin" element={<AdminDepartment />}></Route>
+        <Route path="/dashboard/:tenantId/staff" element={<StaffAdmin />}></Route>
+
+
+
+        {/* Staff-Admin Dashboard */}
+        <Route path="/dashboard/:tenantId/staff/taskManagment" element={<TaskManagment />}></Route>
+        <Route path="/dashboard/:tenantId/staff/taskManagment/operations/tasks" element={<Task />}></Route>
+        <Route path="/dashboard/:tenantId/staff/taskManagment/operations" element={<Operations />}></Route>
+
+
+
+
+        {/* Department-admin Dashboard */}
         <Route path="/dashboard/:tenantId/departmentAdmin/departmentManagment" element={<DepartmentManagment />}></Route>
         <Route path="/dashboard/:tenantId/departmentAdmin/userManagment" element={<UserManagment />}></Route>
-        <Route path="/dashboard/:tenantId/departmentAdmin/operations" element={<Operations />}></Route>
 
-
+        {/*Oranization-admin Dashboard */}
         <Route path="/admin/:tenantId" element={<AdminDashboard />}></Route>
         <Route path="/admin/:tenantId/users" element={<AdminUsersManagment />}></Route>
 
+
+
+        {/* Super-admin Dashboard */}
         <Route path="/superadmin" element={<SuperAdmin />}></Route>
       </Routes>
     </>
